4ef1568506dac5b45a71229af3e11e9d64f69373
Omkar Bhat <odb6pz@virginia.edu>
2020-08-13 19:50:29 -0400

    Update GUISupport.py

--- GUISupport.py
+++ GUISupport.py
@@ -2,12 +2,7 @@ import tkinter as tk
 from tkinter import simpledialog
 from equal_dis import do_sorting
 import pandas as pd
-# from constant import members, keys
 from PIL import ImageTk, Image
-
-# members = ['Ajax', 'King_Arthur', 'Mhota', 'GVK', 'Black_Death', 'Noramay', 'Prof', 'Krakken', 'Shans', 'Lihkin',
-#          'Starks', 'Peacekeeper']
-# keys = [9.5, 6.5, 5, 6, 7, 8, 5, 9, 4, 3.5, 5.5, 3]
 
 HEIGHT = 600
 WIDTH = 800
